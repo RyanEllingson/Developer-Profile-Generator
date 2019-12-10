e3b8e3c0d90e474f0947ae5eacbab56dbea46f82
Geddy Delonge <57271262+RyanEllingson@users.noreply.github.com>
2019-12-10 17:27:42 -0600

    deleted node_modules folder

--- index.js
+++ index.js
@@ -11,8 +11,18 @@ inquirer
 
     axios.get(queryUrl)
     .then(function(response) {
-
         console.log(response.data);
+
+        const picUrl = response.data.avatar_url;
+        const loginName = response.data.login;
+        const location = response.data.location;
+        const gitHubUrl = response.data.url;
+        const blog = response.data.blog;
+        const bio = response.data.bio;
+        const numRepos = response.data.public_repos;
+        const numFollowers = response.data.followers;
+        const numFollowing = response.data.following;
+
     })
     .catch(function(err) {
         console.log(err);
